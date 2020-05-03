50d3586c876cea4e4b2fa615924039b952a8f64e
Roger Sheen <roger@infotexture.net>
2020-05-03 23:55:33 +0200

    Remove obsolete files

--- app/foo.js
+++
@@ -1,7 +0,0 @@
-module.exports = foo => {
-  foo.registerTask('bar', () =>
-    foo.task.run(['task:open_bar', 'task:close_bar']),
-  );
-  const test = ['foo', 'bar', 'hello', 'world'];
-  console.log(test);
-};
